5d6c16cef2c0df9650c6be7b879f445f96d9bf23
shikebazahidi <shikebazahidi@gmail.com>
2025-03-25 08:57:19 -0700

    one function adde

---
+++ app.js
@@ -0,0 +1,13 @@
+let equility= 4
+
+if (equility===4){
+
+    alert("hello your code is right")
+}
+function greeting(ali,mohammad){
+   let resuilt = "ali + mohammad"
+    return resuilt
+    
+}
+alert("hello how are you mohammad","hi,i'm fine thanks ali");
+console.log("hello mohammad" "hello ali");
